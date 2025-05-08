c3dbe7c9ae5e0beb7e05be91fa4c292b45b282e0
Jeff Hykin <jeff.hykin@gmail.com>
2025-05-08 17:32:45 -0500

    -

--- docker_scripts/xstartup.sh
+++ docker_scripts/xstartup.sh
@@ -1,18 +1,38 @@
 #!/bin/sh
-export XKL_XMODMAP_DISABLE=1
+
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
+
+export XKL_XMODMAP_DISABLE=1
+export XDG_CURRENT_DESKTOP="GNOME-Flashback:GNOME"
+export XDG_MENU_PREFIX="gnome-flashback-"
 
 [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
 [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
 xsetroot -solid grey
 vncconfig -iconic &
 
-gnome-session --session=ubuntu
-
-
+gnome-session --builtin --session=gnome-flashback-metacity --disable-acceleration-check --debug &
+nautilus &
+gnome-terminal &
 gnome-panel &
 gnome-settings-daemon &
 metacity &
-nautilus &
-gnome-terminal &
+
+# export XKL_XMODMAP_DISABLE=1
+# unset SESSION_MANAGER
+# unset DBUS_SESSION_BUS_ADDRESS
+
+# [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+# [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+# xsetroot -solid grey
+# vncconfig -iconic &
+
+# gnome-session --session=ubuntu
+
+
+# gnome-panel &
+# gnome-settings-daemon &
+# metacity &
+# nautilus &
+# gnome-terminal &
