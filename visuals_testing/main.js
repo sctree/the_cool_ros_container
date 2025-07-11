eab350a93c284a7069c454bc6caedd5057918785
sctree <seanc0708@gmail.com>
2025-07-11 16:04:35 -0500

    count flag

--- visuals_testing/main.js
+++ visuals_testing/main.js
@@ -1,6 +1,7 @@
 let previousMessage = null
-previousMessage.timestamp = 0
+//previousMessage.timestamp = 0
 let timelapsed = 0
+let count = 0;
 
 function whenStatusUpdateTopicGiven(message) {
     let data = message.data
@@ -10,11 +11,14 @@ function whenStatusUpdateTopicGiven(message) {
     let sitting = data.sitting
     let standing = data.standing
 
-    let timelapsed = timelapsed + message.timestamp - previousMessage.timestamp
+    //let timelapsed = timelapsed + message.timestamp - previousMessage.timestamp
 
     console.debug(`message is:`, message)
-    previousMessage = message
-    console.debug('timelapsed is:', timelapsed)
+    console.debug('count: ', count)
+    console.debug('time lapsed: ', timelapsed)
+    count++
+    timelapsed = timelapsed + message.timestamp.getTime()
+    //console.debug('timelapsed is:', timelapsed)
 
     return `moving: ${moving}`
 }
