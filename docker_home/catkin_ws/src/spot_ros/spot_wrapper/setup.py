4944f4acf91bb6de80bf4612f6d4817d9ae07232
Jeff Hykin <jeff.hykin@gmail.com>
2025-07-21 18:08:47 -0500

    -

--- docker_home/catkin_ws/src/spot_ros/spot_wrapper/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="spot_wrapper",
-    version="1.0.0",
-    description="Wrapper for Boston Dynamics Spot SDK",
-    packages=["spot_wrapper"],
-    install_requires=["bosdyn-client", "bosdyn-api", "bosdyn-mission", "bosdyn-core"],
-)
