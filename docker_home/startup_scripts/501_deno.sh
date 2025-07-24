e628da5fbc3e2a060580af7ce86e594ab23ee6b1
Jeff Hykin <jeff.hykin@gmail.com>
2025-07-24 12:22:35 -0500

    fix deno on git clone

--- docker_home/startup_scripts/501_deno.sh
+++ docker_home/startup_scripts/501_deno.sh
@@ -1,2 +1,9 @@
 export DENO_INSTALL="$HOME/.deno"
 export PATH="$PATH:$DENO_INSTALL/bin"
+# if deno doesnt exist
+if [ -z "$(command -v "deno")" ]
+then
+    # NOTE: DENO_VERSION is set in the Dockerfile
+    # download it
+    curl -fsSL https://deno.land/install.sh | sh -s v"$DENO_VERSION"
+fi
